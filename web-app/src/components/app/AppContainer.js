79bc2b4e3e611c9afe732b23be8c857d199a943d
xy-owski <multan.rafal.k@gmail.com>
2017-10-27 17:26:54 +0200

    create directories for components and styles

---
+++ web-app/src/components/app/AppContainer.js
@@ -0,0 +1,19 @@
+import '../../styles/AppContainer.css';
+
+import React, { Component } from 'react';
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <header className="app-container-header">
+        </header>
+        <p className="app-container-intro">
+          To get started, edit <code>src/AppContainer.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AppContainer;
