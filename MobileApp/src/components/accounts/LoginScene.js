f863e1d5593723a2ce29d9f5ad48d5b27b1e4477
xy-owski <multan.rafal.k@gmail.com>
2017-10-28 02:34:11 +0200

    mobile app gets token from API

--- MobileApp/src/components/accounts/LoginScene.js
+++ MobileApp/src/components/accounts/LoginScene.js
@@ -1,5 +1,6 @@
-import { TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
+import AxiosHelper from '../../helpers/AxiosHelper';
 import React from 'react';
 
 export default class LoginScene extends React.Component {
@@ -12,10 +13,38 @@ export default class LoginScene extends React.Component {
     };
   };
 
+  handleButtonPress = () => {
+    AxiosHelper.token(this.state.dataUsername, this.state.dataPassword)
+    .catch(response => {
+      AsyncStorage.token = response.data;
+      alert("token saved: " + response.data)
+    })
+    .catch(error => {
+      alert("fuck you");
+    });
+  };
+
   render = () => (
-    <View>
-      <TextInput onChangeText={(text) => this.setState(prevState => ({ ...prevState, dataUsername: text }))} value={this.state.dataUsername} />
-      <TextInput onChangeText={(text) => this.setState(prevState => ({ ...prevState, dataPassword: text }))} value={this.state.dataPassword} />
-    </View>
+    <View style={styles.container}>
+      <TextInput style={styles.textInput} autoCorrect={false} onChangeText={(text) => this.setState(prevState => ({ ...prevState, dataUsername: text }))} value={this.state.dataUsername} />
+      <TextInput style={styles.textInput} autoCorrect={false} onChangeText={(text) => this.setState(prevState => ({ ...prevState, dataPassword: text }))} value={this.state.dataPassword} />
+        <Button onPress={this.handleButtonPress} style={styles.button} title="Log in" />
+      </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "skyblue"
+  },
+  textInput: {
+    marginBottom: "1%",
+    flex: 0.8,
+    backgroundColor: "white",
+    borderRadius: 10
+  }
+});
--- MobileApp/src/components/app/AppContentRouter.js
+++ MobileApp/src/components/app/AppContentRouter.js
@@ -1,11 +1,12 @@
+import { Scene, Stack } from 'react-native-router-flux';
+
 import LoginScene from '../accounts/LoginScene';
 import React from 'react';
-import { Scene } from 'react-native-router-flux';
 
 export default class AppContentRouter extends React.Component {
  render = () => (
-  <Scene key="app-content">
+  <Stack key="app-content">
     <Scene key="login" component={LoginScene} title="Login" initial={true} />
-  </Scene>
+  </Stack>
  );
 };
